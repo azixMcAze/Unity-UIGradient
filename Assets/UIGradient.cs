6278e74316b2f9c5f8d27c6f1ebf993d36c86b29
azixMcAze <mathieu.leber@gmail.com>
2017-05-19 20:17:24 +0200

    Added ignore ratio option

--- Assets/UIGradient.cs
+++ Assets/UIGradient.cs
@@ -5,44 +5,53 @@ using UnityEngine.UI;
 
 public class UIGradient : BaseMeshEffect
 {
-	// [ColorUsage(true, true, 0f, 8f, 1f/8f, 3f)]
 	public Color m_color1 = Color.white;
-	// [ColorUsage(true, true, 0f, 8f, 1f/8f, 3f)]
 	public Color m_color2 = Color.white;
 	[Range(-180f, 180f)]
 	public float m_angle = 0f;
-	
+	public bool m_ignoreRatio = true;
+
     public override void ModifyMesh(VertexHelper vh)
     {
 		if(enabled)
 		{
 			Rect rect = graphic.rectTransform.rect;
+
+			float angleRad = m_angle * Mathf.Deg2Rad;
+			float sin = Mathf.Sin(angleRad);
+			float cos = Mathf.Cos(angleRad);
+
+			if (!m_ignoreRatio) {
+				float ratio = rect.height / rect.width;
+				cos *= ratio;
+				float norm = Mathf.Sqrt (cos * cos + sin * sin);
+				cos /= norm;
+				sin /= norm;
+			}
+			
+			Vector2 center = new Vector2 (0.5f, 0.5f);
 			UIVertex vertex = default(UIVertex);
 			for (int i = 0; i < vh.currentVertCount; i++) {
 				vh.PopulateUIVertex (ref vertex, i);
-				Vector2 normalizedPosition = GetNormalizedPosition(vertex.position, rect);
-				Vector2 rotatedPosition = Rotate(normalizedPosition - new Vector2(0.5f, 0.5f), m_angle) + new Vector2(0.5f, 0.5f);
-				vertex.color *= Color.Lerp(m_color1, m_color2, rotatedPosition.y);
+				Vector2 normalizedPosition = NormalizedPosition(vertex.position, rect);
+				Vector2 rotatedPosition = Rotate(normalizedPosition - center, cos, sin) + center;
+				vertex.color *= Color.Lerp(m_color2, m_color1, rotatedPosition.y);
 				vh.SetUIVertex (vertex, i);
 			}
 		}
     }
 
-	static Vector2 GetNormalizedPosition(Vector2 position, Rect rect)
+	static Vector2 NormalizedPosition(Vector2 position, Rect rect)
 	{
 		float x = Mathf.InverseLerp (rect.xMin, rect.xMax, position.x);
 		float y = Mathf.InverseLerp (rect.yMin, rect.yMax, position.y);
 		return new Vector2(x, y);
 	}
 
-    static Vector2 Rotate(Vector2 v, float degrees) {
-		float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
-		float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
-		
-		float tx = v.x;
-		float ty = v.y;
-		v.x = (cos * tx) - (sin * ty);
-		v.y = (sin * tx) + (cos * ty);
-		return v;
+	static Vector2 Rotate(Vector2 v, float cos, float sin)
+	{
+		float x = (cos * v.x) - (sin * v.y);
+		float y = (sin * v.x) + (cos * v.y);
+		return new Vector2(x, y);
 	}
 }
