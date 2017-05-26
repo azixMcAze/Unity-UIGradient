3b1e1bcb9193cb88702b95b5709f95cfd8f233c3
azixMcAze <mathieu.leber@gmail.com>
2017-05-26 21:29:46 +0200

    Removed support for m_ignoreRatio

--- Assets/Scripts/UITextGradient.cs
+++ Assets/Scripts/UITextGradient.cs
@@ -10,7 +10,6 @@ public class UITextGradient : BaseMeshEffect
 	public Color m_color2 = Color.white;
 	[Range(-180f, 180f)]
 	public float m_angle = 0f;
-	public bool m_ignoreRatio = true;
 
     public override void ModifyMesh(VertexHelper vh)
     {
@@ -21,14 +20,6 @@ public class UITextGradient : BaseMeshEffect
 			float angleRad = m_angle * Mathf.Deg2Rad;
 			float sin = Mathf.Sin(angleRad);
 			float cos = Mathf.Cos(angleRad);
-
-			if (!m_ignoreRatio) {
-				float ratio = rect.height / rect.width;
-				cos *= ratio;
-				float norm = Mathf.Sqrt (cos * cos + sin * sin);
-				cos /= norm;
-				sin /= norm;
-			}
 			
 			Vector2 center = new Vector2 (0.5f, 0.5f);
 			UIVertex vertex = default(UIVertex);
