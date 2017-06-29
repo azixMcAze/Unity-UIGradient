2a24ff29bc9d006e59627f5894c5772388a43cc1
azixMcAze <mathieu.leber@gmail.com>
2017-06-29 23:04:01 +0200

    Created script to set dirty a graphic

---
+++ Assets/Scripts/SetDirty.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetDirty : MonoBehaviour {
+	public Graphic m_graphic;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_graphic.SetVerticesDirty();
+	}
+}
