4bcdf2f2c0645109b74bc7c6b0bd0bfcfa71e08e
azixMcAze <mathieu.leber@gmail.com>
2017-07-02 17:35:57 +0200

    Optimized color lerp

--- Assets/Scripts/UIGradientUtils.cs
+++ Assets/Scripts/UIGradientUtils.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public static class UIGradientUtils
 {
@@ -41,14 +42,31 @@ public static class UIGradientUtils
 
 	public static Color InterpolatedColor(Vector2 normalizedPosition, Color topLeft, Color topRight, Color bottomLeft, Color bottomRight)
 	{
-		Color top = Color.Lerp(topLeft, topRight, normalizedPosition.x);
-		Color bottom = Color.Lerp(bottomLeft, bottomRight, normalizedPosition.x);
-		return Color.Lerp(bottom, top, normalizedPosition.y);
+		Color top = Color.LerpUnclamped(topLeft, topRight, normalizedPosition.x);
+		Color bottom = Color.LerpUnclamped(bottomLeft, bottomRight, normalizedPosition.x);
+		return Color.LerpUnclamped(bottom, top, normalizedPosition.y);
 	}
 
 	static Vector2[] ms_verticesPositions = new Vector2[] { Vector2.up, Vector2.one, Vector2.right, Vector2.zero };
 	public static Vector2[] VerticePositions
 	{
 		get { return ms_verticesPositions; }
+	}
+
+	public static float InverseLerp (float a, float b, float v)
+	{
+		return a != b ? (v - a) / (b - a) : 0f;
+	}
+
+	public static void Lerp(UIVertex a, UIVertex b, float t, ref UIVertex c)
+	{
+		c.position = Vector3.LerpUnclamped(a.position, b.position, t);
+		c.normal = Vector3.LerpUnclamped(a.normal, b.normal, t);
+		c.color = Color32.LerpUnclamped(a.color, b.color, t);
+		c.tangent = Vector3.LerpUnclamped(a.tangent, b.tangent, t);
+		c.uv0 = Vector3.LerpUnclamped(a.uv0, b.uv0, t);
+		c.uv1 = Vector3.LerpUnclamped(a.uv1, b.uv1, t);
+		// c.uv2 = Vector3.LerpUnclamped(a.uv2, b.uv2, t);
+		// c.uv3 = Vector3.LerpUnclamped(a.uv3, b.uv3, t);		
 	}
 }
