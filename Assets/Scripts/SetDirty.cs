058ea33347576f1aa84d5f31f210d9ebb485673b
azixMcAze <mathieu.leber@gmail.com>
2017-07-02 17:07:17 +0200

    Added Reset method

--- Assets/Scripts/SetDirty.cs
+++ Assets/Scripts/SetDirty.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class SetDirty : MonoBehaviour {
 	public Graphic m_graphic;
 	// Use this for initialization
-	void Start () {
-		
+	void Reset () {
+		m_graphic = GetComponent<Graphic>();
 	}
 	
 	// Update is called once per frame
